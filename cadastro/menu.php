f9c8eef5243bfec7c75d47927bca0c0c65e725f4
misterhub <luan_100_0@hotmail.com>
2014-01-07 18:26:04 -0200

    Create menu.php

---
+++ cadastro/menu.php
@@ -0,0 +1,10 @@
+<center>
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover Cadastros</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar Usuários</a>
+<br/><br/>
+</center>
