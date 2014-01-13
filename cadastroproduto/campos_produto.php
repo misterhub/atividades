6d9bb9fd735f5a11659888ea99688ffb8f09af11
misterhub <luan_100_0@hotmail.com>
2014-01-13 18:02:23 -0200

    Create campos_produto.php

---
+++ cadastroproduto/campos_produto.php
@@ -0,0 +1,28 @@
+<fieldset>
+<b>Nome do Produto:</b> <input type="text" name="nome" /><br/>
+
+<b>Produto novo?</b><input type="checkbox" name="aceito" value="Sim" checked="checked"/>
+</fieldset>
+<fieldset>
+<b>Cor do Produto</b><br/>
+Vermelho <input type="radio" name="cor" value="Vermelho" /><br/>
+Amarelo <input type="radio" name="cor" value="Amarelo"/><br/>
+Verde <input type="radio" name="cor" value="Verde"/><br/>
+</fieldset>
+
+<fieldset>
+<b>Prazo de Entrega:</b> <select name="prazo">
+    <option>5 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+<br/>
+<b>Detalhes do Produto:</b>
+<br/>
+<textarea name="detalhes" MAXLENGTH="500">Escreva aqui os detalhes</textarea>
+</fieldset>
+
+
+
+
+
