a49fd0d97bd853b7ac3f4aaff9f52860b7ab3bf8
misterhub <luan_100_0@hotmail.com>
2014-01-13 18:05:18 -0200

    Create form_remover.php

---
+++ cadastroproduto/form_remover.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Remover Produto</title>           
+    </head>
+    <body>
+        
+        <fieldset>
+        <?php
+            require_once("menu.php");
+        ?>
+        <center>
+        <form action="remover.php" method="post">
+            Digite o código do produto para remover:<br/><input type="text" name="id" /> </br>
+            <input type="submit" value="Remover" />
+         
+               
+        </form>
+        </center>
+        
+    </body>
+</html>
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    if(isset($_SESSION["cadastro"])){
+        $cadastro = $_SESSION["cadastro"];
+        
+        foreach($cadastro as $id => $produto){
+            if($produto != null){
+                echo "[$id] => " . $produto["nome"] . "<br/>";
+            }
+        }
+    }
+    else{
+        echo "Não existem pessoas cadastradas";
+    }
+        echo "</fieldset>";
+        
+?>
