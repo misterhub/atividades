2ae2d5b0c59ebea42667ec3c1b1f99f06bb5381f
misterhub <luan_100_0@hotmail.com>
2014-01-13 18:06:07 -0200

    Create menu.php

---
+++ cadastroproduto/menu.php
@@ -0,0 +1,12 @@
+<center>
+<fieldset>
+<a href="index.php">Lista de Produto</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar Produto</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar Produto</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover Produto</a>
+<br/><br/>
+</fieldset>
+</center>
